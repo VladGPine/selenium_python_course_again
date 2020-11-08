f773690acbe27137b65c1dbc8d17c7bc0bf03634
VladGPine <shikarno_3000@inbox.ru>
2020-11-09 00:14:14 +0300

    markers part one from lesson 2 from block 3.5 added into test_fixture2.py

--- test_fixture2.py
+++ test_fixture2.py
@@ -29,10 +29,13 @@ def prepare_data():
 
 
 class TestMainPage1:
+
+    @pytest.mark.smoke
     def test_guest_should_see_login_link(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector('#login_link')
 
+    @pytest.mark.regression
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
