dbc67c7d2f430dca1384785a8a7c40c6a6eb2d3c
VladGPine <shikarno_3000@inbox.ru>
2020-11-09 00:57:06 +0300

    lessons with xfail markers addes from block 3.5

--- test_xfail1.py
+++ test_xfail1.py
@@ -29,8 +29,11 @@ class TestMainPage1:
         browser.get(link)
         browser.find_element_by_css_selector('#login_link')
 
-    @pytest.mark.smoke
-    @pytest.mark.win10
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+    @pytest.mark.xfail
+    def test_guest_should_see_search_button_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector('button.favorite')
