9a8726c2695c84e8f0978799aa31babbc91d38b0
VladGPine <shikarno_3000@inbox.ru>
2020-11-09 00:23:47 +0300

    task about marks part two from lesson 3 from block 3.5 added

---
+++ test_fixture_and_marks2.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pytest
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture
+def browser():
+    print("\nstart browser for test..")
+
+    # for linux
+    chrome_options = Options()
+    chrome_options.add_argument("no-sandbox")
+    chrome_options.add_argument("--disable-extensions")
+    browser = webdriver.Chrome('../environments/selenium_env/bin/chromedriver', options=chrome_options)
+
+    # for macOS and windows
+    # self.browser = webdriver.Chrome()
+    yield browser
+    print('\nquit browser..')
+    browser.quit()
+
+
+@pytest.fixture(autouse=True)
+def prepare_data():
+    print()
+    print('preparing some critical data for every test')
+
+
+class TestMainPage1:
+
+    @pytest.mark.smoke
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector('#login_link')
+
+    @pytest.mark.smoke
+    @pytest.mark.win10
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector(".basket-mini .btn-group > a")
